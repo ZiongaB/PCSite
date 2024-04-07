9ea6cbdf9f47c3c4b6f85546dafbe508d5baa23e
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2024-04-07 19:21:00 -0400

    StoryPage Layout Updated and Images Found

--- src/app/storypages/storypage/storypage.component.ts
+++ src/app/storypages/storypage/storypage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Router,RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-storypage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf, RouterOutlet, RouterLink],
   templateUrl: './storypage.component.html',
   styleUrl: './storypage.component.scss'
 })
