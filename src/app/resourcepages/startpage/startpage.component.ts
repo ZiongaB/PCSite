ad895ff4d6a2c521014a41e614acc6055d51b294
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2024-04-09 18:58:37 -0400

    Storypage complete, skeleton for startpage set up

--- src/app/resourcepages/startpage/startpage.component.ts
+++ src/app/resourcepages/startpage/startpage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Router,RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-startpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf, RouterOutlet, RouterLink],
   templateUrl: './startpage.component.html',
   styleUrl: './startpage.component.scss'
 })
