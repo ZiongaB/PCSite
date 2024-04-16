3bc6bac59f974176b42432ea22882a0350298560
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2024-04-16 19:04:45 -0400

    Updated resources Page

--- src/app/resourcepages/partspage/partspage.component.ts
+++ src/app/resourcepages/partspage/partspage.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Router,RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-partspage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf, RouterOutlet, RouterLink],
   templateUrl: './partspage.component.html',
   styleUrl: './partspage.component.scss'
 })
--- src/app/resourcepages/resources/resources.component.ts
+++ src/app/resourcepages/resources/resources.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Router,RouterOutlet, RouterLink } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resources',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf, RouterOutlet, RouterLink],
   templateUrl: './resources.component.html',
   styleUrl: './resources.component.scss'
 })
